046b16f0d4fd97764f4d885ac0371c97242a1872
koseki <dainaso97@gmailcom>
2015-10-16 03:28:29 +0900

    カスタムセルを複数表示

---
+++ memberListApp/MemberTableViewCell2.swift
@@ -0,0 +1,24 @@
+//
+//  MemberTableViewCell2.swift
+//  memberListApp
+//
+//  Created by 前田 晃良 on 2015/10/16.
+//  Copyright (c) 2015年 A.M. All rights reserved.
+//
+
+import UIKit
+
+class MemberTableViewCell2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
